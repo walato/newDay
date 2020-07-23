c3c3e7dd90678f70d7b410e9af4ba8c876ef433e
walato khalil <wwalato.walato@gmail.com>
2020-07-23 14:24:53 +0200

    lesson 79
    get Inputs from UI

--- app.js
+++ app.js
@@ -11,23 +11,48 @@ var budgetController = (function(){
 // UI Controller
 var UIController =(function(){
 
-    //some code
+    var DOMString ={
+        inputType : '.add__type',
+        inputDescription : '.add__description',
+        inputValue : '.add__value',
+        addBtn : '.add__btn'
+    };
+
+    //get Inputs from UI
+    return{
+        getInput:function(){
+            return {
+                tpye:document.querySelector(DOMString.inputType).value,
+                description : document.querySelector(DOMString.inputDescription).value,
+                value : document.querySelector(DOMString.inputValue).value
+            };
+        },
+        getDOMString : function(){
+            return DOMString;
+        }
+    }; 
 
 })();
 
 // public Controller
-var controller = (function(budgetCont , UICont){
+var controller = (function(budgetCrtl , UICrtl){
+    
+    var DOM = UICrtl.getDOMString();
 
     function ctrlAddEvent(){
+        
         // 1 get the field input
+        var input= UICrtl.getInput();
+        console.log(input);
+        
         // 2 add the item to budget controller
         // 3 add the item to UI
         // 4 calculate budget 
         // 5 display budget on UI
-        console.log("heh");
+        
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddEvent);
+    document.querySelector(DOM.addBtn).addEventListener('click', ctrlAddEvent);
 
     document.addEventListener('keypress',function(e){
         if(e.keyCode===13){
